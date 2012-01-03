60e8685299e1762b61a3a06d6347f08a9881b578
LucasAnderson <lucas@lucasanderson.com>
2012-01-03 15:11:13 -0600

    Updated install for single pages.

--- controller.php
+++ controller.php
@@ -5,15 +5,15 @@ class ApiPackage extends Package {
 	protected $pkgHandle = 'api';
 	protected $appVersionRequired = '5.5.0';
 	protected $pkgVersion = '1.0';
-	
+
 	public function getPackageName() {
 		return t("Api");
 	}
-	
+
 	public function getPackageDescription() {
-		return t("Provides an external api.");
+		return t("Provides an external API for remote management of your site. Allows integration into 3rd party applications.");
 	}
-	
+
 	public function on_start() {
 		if(!defined('BASE_API_PATH')) {
 			define('BASE_API_PATH', '-/api');
@@ -23,15 +23,19 @@ class ApiPackage extends Package {
 		//possibly on_start if we are using a db
 		Events::extend('on_before_render', 'ApiRequest', 'parseRequest', DIR_PACKAGES.'/'.$this->pkgHandle.'/'.DIRNAME_MODELS.'/api_routes.php');
 	}
-	
+
 	public function install() {
+		$pkg = parent::install();
 		$vh = Loader::helper('validation/identifier');
 		$key = $vh->getString(24);
-		$pkg = $this;
 		$pkg->saveConfig('key', $key);
-		parent::install();
+		Loader::model('single_page');
+		$p = SinglePage::add('/dashboard/api',$pkg);
+		$p->update(array('cName'=>t('concrete5 API'), 'cDescription'=>t('Remote management of your site.')));
+		$p2 = SinglePage::add('/dashboard/api/manage_routes',$pkg);
+		$p2->update(array('cName'=>t('Manage Routes'), 'cDescription'=>t('Managed installed API routes.')));
 	}
-	
+
 	public function uninstall() {
 		$force = $_POST['force'];
 		if($force != t('remove')) {
@@ -44,7 +48,7 @@ class ApiPackage extends Package {
 				throw new Exception(t('Please uninstall all addons that register routes with the API, before uninstalling this addon.'));
 			}
 		}
-		parent::uninstall();
+		$pkg = parent::uninstall();
 	}
 
 }
