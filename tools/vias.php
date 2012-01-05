461f9004d4d0eef8cb24064b0b60d61924cef5ab
Mnkras <mnkras@gmail.com>
2012-01-05 11:35:16 -0500

    more work on the via dialog

--- tools/vias.php
+++ tools/vias.php
@@ -18,17 +18,25 @@ if(!is_object($api)) {
 //print_r($api);
 $via = $api->getVia();
 $enabled = $api->getViaEnabled();
+echo '<div class="ccm-ui">';
+echo '<form method="post" action="">';
+echo '<input type="hidden" name="ID" value="'.$id.'"/>';
+echo '<table>';
 foreach($via as $type) {
+	echo '<tr>';
 	if(isset($enabled[$type])) {
 		$checked = ' checked="checked"';
 	} else {
 		$checked = '';
 	}
-	echo '<input id="'.strtoupper($type).'" type="checkbox" name="'.strtoupper($type).'"'.$checked.'/><label for="'.strtoupper($type).'">'.strtoupper($type).'</label>';
+	echo '<td><input id="'.strtoupper($type).'" type="checkbox" name="'.strtoupper($type).'"'.$checked.'/></td><td><label for="'.strtoupper($type).'">'.strtoupper($type).'</label></td>';
+	echo '</tr>';
 }
 ?>
-
+</table>
 <div class="dialog-buttons">
 	<input type="button" onclick="jQuery.fn.dialog.closeTop()" class="btn" value="<?php echo t('Cancel')?>"/>
 	<input type="button" class="btn primary ccm-button-v2-right" value="<?php echo t('Save')?>"/>
+</div>
+</form>
 </div>
