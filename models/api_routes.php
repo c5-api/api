b2fba5fa026d19075ca9c02131cc58c80b041c50
Mnkras <mnkras@gmail.com>
2012-01-08 13:59:20 -0500

    send the correct header

--- models/api_routes.php
+++ models/api_routes.php
@@ -209,6 +209,51 @@ class ApiResponse {
 	private $logging = false;//does nothing so far, log requests and data returned?
 
 	/**
+	 * @var string $header
+	 */
+	 private $header = 'OK';
+
+	/**
+	 * @var array $codes
+	 */
+	private $codes = array(
+		'100' => 'Continue',
+		'200' => 'OK',
+		'201' => 'Created',
+		'202' => 'Accepted',
+		'203' => 'Non-Authoritative Information',
+		'204' => 'No Content',
+		'205' => 'Reset Content',
+		'206' => 'Partial Content',
+		'300' => 'Multiple Choices',
+		'301' => 'Moved Permanently',
+		'302' => 'Found',
+		'303' => 'See Other',
+		'304' => 'Not Modified',
+		'305' => 'Use Proxy',
+		'307' => 'Temporary Redirect',
+		'400' => 'Bad Request',
+		'401' => 'Unauthorized',
+		'402' => 'Payment Required',
+		'403' => 'Forbidden',
+		'404' => 'Not Found',
+		'405' => 'Method Not Allowed',
+		'406' => 'Not Acceptable',
+		'409' => 'Conflict',
+		'410' => 'Gone',
+		'411' => 'Length Required',
+		'412' => 'Precondition Failed',
+		'413' => 'Request Entity Too Large',
+		'414' => 'Request-URI Too Long',
+		'415' => 'Unsupported Media Type',
+		'416' => 'Requested Range Not Satisfiable',
+		'417' => 'Expectation Failed',
+		'500' => 'Internal Server Error',
+		'501' => 'Not Implemented',
+		'503' => 'Service Unavailable'
+	);
+
+	/**
 	 * Gets the current api response object
 	 *
 	 * @return ApiResponse
@@ -283,6 +328,7 @@ class ApiResponse {
 	 * @return void
 	 */		
 	public function setCode($data = 200) {
+		$this->header = $this->codes[$data];
 		$this->code = $data;
 	}
 
@@ -306,7 +352,8 @@ class ApiResponse {
 	 *
 	 * @return void
 	 */	
-	public function send() {
+	public function send() {	
+		header("HTTP/1.1 ".$this->code." ".$this->header);
 		if($this->format == 'xml' && class_exists('XMLWriter')) {
 			echo $this->sendXml();
 		} else if($this->format == 'html' && $this->debug) {
