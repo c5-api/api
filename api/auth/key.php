8731b16690f57612d2f896af9adab9c537768d1f
Mnkras <mnkras@gmail.com>
2012-08-26 19:02:11 -0400

    more auth stuff

--- api/auth/key.php
+++ api/auth/key.php
@@ -1,5 +1,16 @@
 <?php defined('C5_EXECUTE') or die('Access Denied');
 
 class KeyApiAuth extends ApiAuthModel {
+
+	public function authorize() {
+		if($_REQUEST['key'] == '12345') {
+			return true;
+		}
+		return false;
+	}
+
+	public function getKeys() {
+
+	}
 	
 }
--- models/api/auth/model.php
+++ models/api/auth/model.php
@@ -1,5 +1,39 @@
 <?php defined('C5_EXECUTE') or die('Access Denied');
 
-class ApiAuthModel {
+class ApiAuthModel extends ADOdb_Active_Record {
+
+	public $_table = 'ApiAuth';
+
+	public function __construct() {
+		parent::__construct('ApiAuth', array('aID'))
+	}
+
+	public static function getByID($ID) {
+		$route = new ApiAuth();
+		$route->Load('aID = ?', array($ID))
+		return $route;
+	}
+
+	public static function getByHandle($ID) {
+		$route = new ApiAuth();
+		$route->Load('handle = ?', array($ID))
+		return $route;
+	}
+
+	public static function add($handle, $pkg, $enabled = true) {
+		if(is_string($pkg)) {
+			$pkg = Package::getByHandle($pkg);
+		}
+		$rt = new ApiAuth();
+		$rt->pkgID = $pkg->getPackageID();
+		$rt->handle = $handle;
+		$rt->enabled = $enabled;
+		$rt->save();
+		return $rt;
+	}
 	
+	public function authorize() {
+		return true;
+	}
+
 }
